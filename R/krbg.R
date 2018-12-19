6b12ff0b9ae91dd4c601d9001f483bbe61f054c2
Chanyub.park <mrchypark@gmail.com>
2018-12-20 01:46:21 +0900

    init pack

---
+++ R/krbg.R
@@ -0,0 +1,7 @@
+#' 2019 Korean Budget Data
+#'
+#' @source http://newslabit.hankyung.com/article/201812187348G
+#' @format A "tibble" ("tidy" data frame) with 1,590 rows and 15 variables:
+#' @examples
+#'   krbg
+"krbg"
---
+++ data-raw/krbg.R
@@ -0,0 +1,19 @@
+tar <- "https://docs.google.com/spreadsheets/d/1bJUcmPADUHB5DuPVkD71DjezAajCj6vip_n4TZRPUXA/export?format=csv"
+download.file(tar, destfile = "./data-raw/in.csv")
+
+tar <- "https://docs.google.com/spreadsheets/d/1ejyHJZC62c9CSSJ4WOlUAHG1P80L4iAXx3qCCKiPobU/export?format=csv"
+download.file(tar, destfile = "./data-raw/out.csv")
+
+library(readr)
+ind <- read_csv("data-raw/in.csv")
+outd <- read_csv("data-raw/out.csv")
+
+library(dplyr)
+ind %>%
+  mutate(`장(분야)` = "") %>%
+  rename(`관(부문)` = `관`,
+         `항(프로그램)` = `항`) %>%
+  bind_rows(outd) %>%
+  select(`소관`, `입출분류`, `회계분류`, `계정`, `장(분야)`, everything()) -> krbg
+
+usethis::use_data(krbg)
